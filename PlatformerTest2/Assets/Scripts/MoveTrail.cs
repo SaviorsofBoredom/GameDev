73f021b4d1cba9876d3ab910c7733e2ca7399758
BJSherman <benjamin.sherman.uh@gmail.com>
2015-10-06 22:10:42 -0500

    Test
    The test level. I have not finished the scaling yet.

---
+++ PlatformerTest2/Assets/Scripts/MoveTrail.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTrail : MonoBehaviour
+{
+
+    public int moveSpeed = 230;
+
+	void Update ()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+        Destroy(gameObject, 1);
+	}
+}
---
+++ PlatformerTest2/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	public float delayTime = 5;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds (delayTime);
+
+		Application.LoadLevel (1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
